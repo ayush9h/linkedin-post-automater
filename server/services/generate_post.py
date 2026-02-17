512a7c77cee2bc2b928385c4eacfe0c0e4d7b3f5
Ayush <ayush.kumar.21cse@bmu.edu.in>
2026-02-17 22:07:29 +0530

    add: placeholder for websearch agent and conditional routing

--- server/services/generate_post.py
+++ server/services/generate_post.py
@@ -7,14 +7,27 @@ content_generation_agent = ContentGenerationAgent(
     model_client=model_client,
 )
 
+web_search_agent = WebSearchAgent(name="WebSearchAgent")
+
 prompt_improvisation_agent = PromptImprovisationAgent(
     name="PromptImprovisationAgent",
     model_client=model_client,
 )
 
 builder: DiGraphBuilder = DiGraphBuilder()
-builder.add_node(content_generation_agent).add_node(prompt_improvisation_agent)
+builder.add_node(content_generation_agent).add_node(
+    prompt_improvisation_agent
+).add_node(web_search_agent)
+
+
+# Workflow
+builder.add_edge(
+    prompt_improvisation_agent, web_search_agent, condition=wants_web_search
+)
+
 builder.add_edge(prompt_improvisation_agent, content_generation_agent)
+builder.add_edge(web_search_agent, content_generation_agent)
+
 
 graph = builder.build()
 flow: GraphFlow = GraphFlow(
